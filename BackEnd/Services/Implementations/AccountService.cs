40d51faf0b3821a553cb6095b6d24f0d05b7ed88
Plamen Dobrev <46189922+ApPlamen@users.noreply.github.com>
2021-04-14 00:48:28 +0300

    Remove NotAuthorizedException exception

--- BackEnd/Services/Implementations/AccountService.cs
+++ BackEnd/Services/Implementations/AccountService.cs
@@ -105,7 +105,7 @@ namespace Services
             }
             catch (SecurityTokenException)
             {
-                throw new NotAuthorizedException();
+                throw new UnauthorizedAccessException("Not Authorized");
             }
         }
 
