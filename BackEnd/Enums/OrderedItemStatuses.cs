6b5d33bff700ca3de780812f561635e637f92632
Plamen Dobrev <46189922+ApPlamen@users.noreply.github.com>
2021-05-20 17:52:15 +0300

    backend setted up

---
+++ BackEnd/Enums/OrderedItemStatuses.cs
@@ -0,0 +1,11 @@
+﻿namespace Enums
+{
+    public enum OrderedItemStatuses
+    {
+        New = 1,
+        Taken = 2,
+        Served = 3,
+        AskedToRemove = 4,
+        Removed = 5,
+    }
+}
---
+++ BackEnd/Services/Implementations/ItemOrderManagementService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using DAL.InputModels;
+using DAL.Models;
+using DAL.Repository;
+using Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace Services
+{
+    class ItemOrderManagementService : BaseService<MenuItemOrder>, IItemOrderManagementService
+    {
+        public ItemOrderManagementService(IMapper mapper,
+            IRepository<MenuItemOrder> userOrder,
+            UserManager<User> userManager)
+            : base(mapper, userOrder, userManager)
+        { }
+
+        public void SetOrderedItemStatus(OrderedItemStatusInputModel model)
+        {
+            var itemOrdered = this.repo.GetById(model.ItemId);
+
+            itemOrdered.OrderedItemStatus = (OrderedItemStatuses)model.Status.Value;
+
+            this.repo.Save();
+        }
+
+        public void AskToRemove(OrderedItemStatusInputModel model)
+        {
+            var itemOrdered = this.repo.GetById(model.ItemId);
+
+            itemOrdered.OrderedItemStatus = OrderedItemStatuses.AskedToRemove;
+
+            this.repo.Save();
+        }
+    }
+}
